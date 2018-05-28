dde8fda87748f5d0eccc6fc94793be45cefc40c4
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2018-05-28 01:22:32 -0700

    added main.h

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include "FunctionalClock.h"
 
-#include <vector>
-#include <random>
-#include <algorithm>
-#include <iostream>
-void fillVector(std::vector<int>& v, const unsigned int& SIZE);
-
+#include "main.h"
 
 int main()
 {
@@ -27,14 +22,3 @@ int main()
 }
 
 
-void fillVector(std::vector<int>& v, const unsigned int& SIZE)
-{
-    
-		std::default_random_engine generator;
-		std::uniform_int_distribution<int> distribution(0, (SIZE * 9) );
-
-		for (unsigned int i = 0; i < SIZE; ++i)
-		{
-            v.push_back(distribution(generator));
-		}
-}
